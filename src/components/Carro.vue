dc24033fa096a13b70b80a0c5d4cf7a92dfb28bd
jeison003 <jeisonmartinezev@unimagdalena.edu.co>
2024-03-30 17:27:47 -0500

    the pictures are responsives

--- src/components/Carro.vue
+++ src/components/Carro.vue
@@ -12,14 +12,14 @@
 <template>
     <div  class="col-md-6 col-lg-4 my-4 row align-items-center"  >
                 <div class="col-4">
-                    <img class="img-fluid" 
+                    <img class="img-fluid carImg" 
                     :src="carro.imagen" 
                     :alt="'imagen carro' + carro.nombre">
                 </div>
                 <div class="col-8">
-                    <h3 class="text-black fs-4 fw-bold text-uppercase">{{ carro.nombre }}</h3>
+                    <h3 class="text-black fs-5 fw-bold text-uppercase">{{ carro.nombre }}</h3>
                     <p>{{ carro.descripcion }}</p>
-                    <p class="fw-black text-primary fs-3">${{ carro.precio}}</p>
+                    <p class="fw-black text-primary fs-4">${{ carro.precio}}</p>
                     <button
                      type="button"
                       class="btn btn-dark w-100 "
